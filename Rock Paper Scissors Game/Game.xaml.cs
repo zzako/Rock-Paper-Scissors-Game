597cf8cd7b2e9fbfadd4e7e72b5844fd829d371c
Zaakir Ismail <zakmini.isi@gmail.com>
2020-09-24 17:02:09 +0200

    Commit

---
+++ Rock Paper Scissors Game/Game.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Rock_Paper_Scissors_Game
+{
+    /// <summary>
+    /// Interaction logic for Game.xaml
+    /// </summary>
+    public partial class Game : Window
+    {
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+
+
+      
+
+    
+
+        private void OnImageButtonClick3(object sender, RoutedEventArgs e)
+        {
+            List<string> computerHand1 = new List<string>(3);
+            computerHand1.Add("Rock");
+            computerHand1.Add("Paper");
+            computerHand1.Add("Scissors");
+
+            Random random = new Random();
+            int index = random.Next(computerHand1.Count);
+
+            if (index == 0)
+            {
+                MessageBox.Show("You Drew, Computer chose Rock");
+            }
+            else if (index == 1)
+            {
+                MessageBox.Show("You Lost, Computer chose  " + index);
+            }
+            else if (index == 2)
+            {
+                MessageBox.Show("You Won, Computer chose  " + index);
+            }
+        }
+
+        private void OnImageButtonClick2(object sender, RoutedEventArgs e)
+        {
+            List<string> computerHand1 = new List<string>(3);
+            computerHand1.Add("Rock");
+            computerHand1.Add("Paper");
+            computerHand1.Add("Scissors");
+
+            Random random = new Random();
+            int index = random.Next(computerHand1.Count);
+
+            if (index == 0)
+            {
+                MessageBox.Show("You Won, Computer chose Rock ");
+            }
+            else if (index == 1)
+            {
+                MessageBox.Show("You Drew, Computer chose Paper ");
+            }
+            else if (index == 2)
+            {
+                MessageBox.Show("You Lost, Computer chose Scissors");
+            }
+        }
+
+        private void OnImageButtonClick1(object sender, RoutedEventArgs e)
+        {
+            List<string> computerHand1 = new List<string>(3);
+            computerHand1.Add("Rock");
+            computerHand1.Add("Paper");
+            computerHand1.Add("Scissors");
+
+            Random random = new Random();
+            int index = random.Next(computerHand1.Count);
+
+            if (index == 0)
+            {
+                MessageBox.Show("You Drew, Computer chose Rock ");
+            }
+            else if (index == 1)
+            {
+                MessageBox.Show("You Lost, Computer chose Paper ");
+            }
+            else if (index == 2)
+            {
+                MessageBox.Show("You Won, Computer chose Scissors ");
+            }
+        }
+    }
+}
---
+++ Rock Paper Scissors Game/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rock_Paper_Scissors_Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Game open = new Game();
+            open.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
